0dd1b7cbd6885c8c6574fcbf1ab5e0f1f6454da1
sujeethes <44137913+sujeethes@users.noreply.github.com>
2021-01-18 14:21:43 +0530

    Changed Account
    Email Validation DOne

--- app/src/main/java/com/example/furnico/AccountOptions.java
+++ app/src/main/java/com/example/furnico/AccountOptions.java
@@ -10,7 +10,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class AccountOptions extends AppCompatActivity {
-
+//Change in FIle
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
